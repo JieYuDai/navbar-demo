bcef40876801038261ae9c4137e33c72d137f0d6
JieYuDai <2541769887@qq.com>
2021-07-17 16:58:01 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav-bar></nav-bar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavBar from '@/components/navbar'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/navbar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="nav-comps">
+    <nav class="clearfix">
+      <span href="#" id="pull">Menu</span>
+      <ul class="clearfix" ref="navbar">
+        <li><img src="../assets/logo.png" style="margin-top: 4px;"></li>
+        <li><span :class="active === '/home' ? 'active' : ''">首页</span></li>
+        <li><span :class="active === '/product' ? 'active' : ''">产品介绍</span></li>
+        <li><span :class="active === '/case' ? 'active' : ''">案例分享</span></li>
+        <li><span :class="active === '/news' ? 'active' : ''">新闻动态</span></li>
+        <li><span :class="active === '/about' ? 'active' : ''">关于XX</span></li>
+        <li><span :class="active === '/contact' ? 'active' : ''">联系我们</span></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      active: '/home'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.nav-comps {
+  nav {
+    height: 60px;
+    position: fixed;
+    width: 100%;
+    background: #FFF;
+    font-size: 15px;
+    font-family: 'PT Sans', Arial, sans-serif;
+    font-weight: bold;
+    border-bottom: 1px solid #eeeeee;
+    ul {
+      display: table;
+      height: 60px;
+      margin: 0 auto;
+      li {
+        display: inline;
+        float: left;
+        &:first-child {
+          margin-right: 120px;
+        }
+        span {
+          color: #333;
+          display: inline-block;
+          width: 100px;
+          text-align: center;
+          text-decoration: none;
+          line-height: 60px;
+          cursor: pointer;
+          &:hover {
+            color: #1896D3;
+          }
+        }
+        .active {
+          color: #1896D3;
+        }
+      }
+    }
+  }
+}
+</style>
